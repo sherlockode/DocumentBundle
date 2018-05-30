9e82b8518cb4f17d17ca80e0519faf68d3b4869d
Julien Chichignoud <julien.chichignoud@gmail.com>
2018-05-30 19:26:26 +0200

    Initial commit

---
+++ SherlockodeDocumentBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sherlockode\DocumentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SherlockodeDocumentBundle
+ */
+class SherlockodeDocumentBundle extends Bundle
+{
+
+}
